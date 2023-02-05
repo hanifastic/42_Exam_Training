bf55b1451c4f33f95bed981715110debb4af3b41
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-02-05 18:47:03 +0300

    Update ft_rrange.c

--- Level_03/ft_rrange/ft_rrange.c
+++ Level_03/ft_rrange/ft_rrange.c
@@ -1,3 +1,76 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int *ft_rrange(int start, int end)
+{
+    int *num;
+    int birim;
+    int i;
+    i = 0;
+    if(start > end)
+    {
+        birim = start - end + 1;
+        num = (int *)malloc(sizeof(int) * birim);
+        while(i < birim)
+        {
+            num[i] = end;
+            i++;
+            end++;
+        }
+    }
+    // else if(start < end)
+    // {
+
+    // }
+    // else if(start == end)
+    // {
+    //     return(&start);
+    // }
+
+    return(num);
+}
+
+int main()
+{
+    int start = 3;
+    int end = 1;
+    int i = 0;
+    int birim = 3;
+    int *gelen;
+    gelen = ft_rrange(start,end);
+    while(i < birim)
+    {
+        printf("%i\n",gelen[i]);
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <stdlib.h>
 
 int		ft_abs(int x)
