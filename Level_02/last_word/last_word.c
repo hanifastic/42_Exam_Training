1f1858ef4e2de30c6bae1c6dc53e8823888e4dee
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-02-05 14:27:26 +0300

    Update last_word.c

--- Level_02/last_word/last_word.c
+++ Level_02/last_word/last_word.c
@@ -1,5 +1,60 @@
 #include <unistd.h>
 
+int main(int ac, char **av)
+{
+    int i;
+    i = 0;
+    if(ac==2)
+    {
+        while(av[1][i] != '\0')
+            i++;
+        i--;
+        while(av[1][i] == ' ' || av[1][i] == '\t')
+            i--;
+        while(av[1][i] != ' ' && av[1][i] != '\t' && av[1][i])
+            i--;
+        i++;
+        while(av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\0')
+        {
+            write(1,&av[1][i],1);
+            i++;
+        } 
+    }
+    write(1,"\n",1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int		main(int ac, char **av)
 {
 	int i;
