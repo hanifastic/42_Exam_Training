5e5c7c1426f088dc2ab3f164ed98b6b7d21a7c47
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-05 15:53:14 +0300

    Update rstr_capitalizer2.c

--- Level_03/rstr_capitalizer/rstr_capitalizer2.c
+++ Level_03/rstr_capitalizer/rstr_capitalizer2.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
-void rstr_capitalizer(char *str)
+void r_str(char *str)
 {
     int i = 0;
     while(str[i])
     {
         if(str[i] >= 'A' && 'Z' >= str[i])
             str[i] += 32;
-        if((str[i] >= 'a' && 'z' >= str[i]) && (str[i + 1] == ' ' || 
+        if((str[i] >= 'a' && 'z' >= str[i]) && (str[i + 1] == ' ' ||
          str[i + 1] == '\t' || str[i + 1] == '\0'))
             str[i] -= 32;
         write(1,&str[i++],1);
@@ -16,15 +16,15 @@ void rstr_capitalizer(char *str)
 
 int main(int ac, char **av)
 {
-    int i;
+    int i = 1;
     if(ac > 1)
     {
-        i = 1;
         while(i < ac)
         {
-            rstr_capitalizer(av[i]);
-            i++;
+            r_str(av[i++]);
+            write(1,"\n",1);
         }
     }
-    write(1,"\n",1);
+    else
+        write(1,"\n",1);
 }
