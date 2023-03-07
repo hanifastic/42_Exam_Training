9e8ccc6684a8be669ed31360ba97652df95492df
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-07 11:57:54 +0300

    Update rev_wstr2.c

--- Level_04/rev_wstr/rev_wstr2.c
+++ Level_04/rev_wstr/rev_wstr2.c
@@ -2,29 +2,34 @@
 
 int main(int ac, char **av)
 {
-    int start;
-    int end;
-    int i = 0;
-    int flg = 1;
-
-    if(ac==2)
-    {
-        while(av[1][i])
-            i++;
-        while(i >= 0)
-        {
-            while(av[1][i] == '\0' || av[1][i] == ' ' || av[1][i] == '\t')
-                i--;
-            end = i;
-            while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
-                i--;
-            start = i + 1;
-            flg = start;
-            while(start <= end)
-                write(1,&av[1][start++],1);
-            if(flg != 0)
-                write(1," ",1);
-        }
-    }
-    write(1,"\n",1);
+	int start;
+	int end;
+	int i = 0;
+	int flg = 1;
+	
+	if(ac == 2)
+	{
+		while(av[1][i] != '\0')
+			i++;
+		while(i >= 0)
+		{
+			while(av[1][i] == '\0' ||av[1][i] == ' ' ||av[1][i] == '\t')
+				i--;
+			end = i;
+			while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+				i--;
+			start = i + 1;
+			flg = start;
+			while(start <= end)
+			{
+				write(1,&av[1][start],1);
+				start++;
+			}
+			if(flg != 0)
+			{
+				write(1," ",1);
+			}
+		}
+	}
+	write(1,"\n",1);
 }
