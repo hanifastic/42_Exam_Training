695a5b1ce5388300ca04b71b01abde7bd10da415
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-07 17:39:59 +0300

    Update inter2.c

--- Level_02/inter/inter2.c
+++ Level_02/inter/inter2.c
@@ -5,21 +5,35 @@ int main(int ac, char **av)
     if(ac == 3)
     {
         int i = 0;
-        int j = 0;
-        char control[256] = {0};
+        int i2 = 0;
+        char kontrol[256] = {0};
         while(av[1][i])
         {
-            j = 0;
-            while(av[2][j])
+            i2 = 0;
+            while(av[2][i2])
             {
-                if(av[1][i] == av[2][j] && control[(int)av[1][i]] != 1)
+                if(kontrol[(int)av[1][i]] != 1)
                 {
-                    control[(int)av[1][i]] = 1;
+                    kontrol[(int)av[1][i]] = 1;
                     write(1,&av[1][i],1);
                 }
-                j++;
+                i2++;
             }
             i++;
+        }
+        while(av[2][i2])
+        {
+            i2 = 0;
+            while(av[1][i])
+            {
+                if(kontrol[(int)av[2][i2]] != 1)
+                {
+                    kontrol[(int)av[1][i]] = 1;
+                    write(1,&av[2][i2],1);
+                }
+                i++;
+            }
+            i2++;
         }
     }
     write(1,"\n",1);
