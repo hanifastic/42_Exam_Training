0eecd04bf5d9298842c18e781267b88acd8be492
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-10 04:06:21 +0300

    Update pgcd.c

--- Level_03/pgcd/pgcd.c
+++ Level_03/pgcd/pgcd.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	pgcd(int a, int b)
+int pgcd(unsigned int a, unsigned int b)
 {
-	int i;
-	int gcd;
-
-	i = 1;
-	gcd = 0;
-	while (i < a && i < b)
-	{
-		if (a % i == 0 && b % i == 0)
-			gcd = i;
-		i++;
-	}
-	printf("%d", gcd);
+    int i = a;
+    while(i)
+    {
+        if(a % i == 0 && b % i == 0)
+            return(i);
+        i--;
+    }
 }
 
-int		main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac == 3)
-		pgcd(atoi(av[1]), atoi(av[2]));
-	printf("\n");
-	return (0);
+    if(ac==3)
+    {
+        int a = atoi(av[1]);
+        int b = atoi(av[2]);
+
+        if(a <= 0 || b <= 0)
+            return(0);
+        printf("%d",pgcd(a,b));
+    }
+    printf("\n");
 }
