253484fccda9caa13e9467a55b5b5f91f71d24f7
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-10 11:33:13 +0300

    Update union2.c

--- Level_02/union/union2.c
+++ Level_02/union/union2.c
@@ -2,11 +2,11 @@
 
 int main(int ac, char **av)
 {
-    if(ac == 3)
+    int i = 0;
+    int i2 = 0;
+    char kontrol[256] = {0};
+    if(ac==3)
     {
-        int i = 0;
-        int i2 = 0;
-        char kontrol[256] = {0};
         while(av[1][i])
         {
             i2 = 0;
@@ -21,14 +21,16 @@ int main(int ac, char **av)
             }
             i++;
         }
+        i = 0;
+        i2 = 0;
         while(av[2][i2])
         {
-            i2 = 0;
+            i = 0;
             while(av[1][i])
             {
                 if(kontrol[(int)av[2][i2]] != 1)
                 {
-                    kontrol[(int)av[1][i]] = 1;
+                    kontrol[(int)av[2][i2]] = 1;
                     write(1,&av[2][i2],1);
                 }
                 i++;
