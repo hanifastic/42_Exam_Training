88f204f9515263a2b013cb33de24e354c731ec68
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-14 09:29:13 +0300

    Update ft_strrev.c

--- Level_02/ft_strrev/ft_strrev.c
+++ Level_02/ft_strrev/ft_strrev.c
@@ -1,37 +1,17 @@
-#include "unistd.h"
-#include "stdio.h"
-int ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
-
 char *ft_strrev(char *str)
 {
-    int i;
-    int len;
-    char tmp;
-
-    i = 0;
-    len = ft_strlen(str) - 1;
-    while (i < len)
-    {
-        tmp = str[i];
-        str[i] = str[len];
-        str[len] = tmp;
-        i++;
-        len--;
-    }
-    return (str);
-}
-
-int main()
-{
-    char str[6] = "araba";
-    ft_strrev(str);
-    printf("%s", str);
+	int i = 0,len = 0;
+	char tmp;
+	while(str[len])
+		len++;
+	len--;
+	while (str[i])
+	{
+		tmp = str[i]
+		str[i] = str[len];
+		str[len] = tmp;
+		i++;
+		len--;
+	}
+	return(str);
 }
