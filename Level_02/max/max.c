f3775355b3cec9c3ff7954d5bdcefd930a5aea8e
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-14 10:10:51 +0300

    Update max.c

--- Level_02/max/max.c
+++ Level_02/max/max.c
@@ -1,18 +1,4 @@
-int max(int *tab, unsigned int len)
-{
-	int maxx = 0;
-	if(!tab)
-		return(0);
-	while(len--)
-	{
-		if(tab[len] > maxx)
-			maxx = tab[len];
-	}
-	return(maxx);
-}
 
-
-/*
 int max(int *tab, unsigned int len)
 {
     if(len == 0)
@@ -26,4 +12,4 @@ int max(int *tab, unsigned int len)
         i++;
     }
     return(result);
-}*/
+}
