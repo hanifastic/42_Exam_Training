30a453780f643351dbe8f733b89634911ad9284c
Hanifi ŞİŞMAN <92511344+hanifastic@users.noreply.github.com>
2023-03-14 06:09:15 +0300

    Update pgcd2.c

--- Level_03/pgcd/pgcd2.c
+++ Level_03/pgcd/pgcd2.c
@@ -1,26 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int ac, char const **av)
+int main(int ac, char **av)
 {
-	int n1 = atoi(av[1]);
-	int n2 = atoi(av[2]);
-  int temporary = 0;
-	if (ac == 3)
-  {
-    if (n1 <= 0 || n2 <= 0)
+    int n1 = atoi(av[1]);
+    int n2 = atoi(av[2]);
+    int temporary = 0;
+    if(ac==3)
     {
-      printf("\n");
-      return 0;
+        if(n1 <= 0 || n2 <= 0)
+            return(0);
+        while(n1 != 0)
+        {
+            temporary = n1;
+            n1 = n1 % n2;
+            n2 = temporary;
+        }
+        printf("%d\n",n2);
     }
-    while (n2 != 0)
-    {
-      temporary = n2;
-      n2 = n1 % n2;
-      n1 = temporary;
-    }
-    printf("%d\n", n1);
-  }
-  else
-     printf("\n");
+    else
+        printf("\n");
 }
